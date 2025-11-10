29c32c1488c6dc952dcbc6d6ff8b90007159770a
Corey Matt <corey.matt@student.swic.edu>
2025-11-09 23:16:40 -0600

    Add select internal links and append internal to text

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
+// Select Header
 const mainHeader = document.getElementById("main-header");
 mainHeader?.addEventListener("click", () => {
   console.info(mainHeader);
 });
 
+// get number of nav links
 const links = document.querySelectorAll(".navbar a");
 console.info("Number of links in navbar:", links.length);
+
+// Select internal links (those starting with /) and append " [internal]" to their text.
+const internalLinks = document.querySelectorAll('a[href^="/"]');
+internalLinks.forEach((link) => {
+  link.textContent += " [internal]";
+});
